03a9e505616e2653065b5fd8d6e0191cf4d9b4ec
Hassan Abbas <95937687+HassanAbbas10@users.noreply.github.com>
2024-07-10 16:11:59 +0500

    Initial commit

--- src/components/Header/HeaderSec.jsx
+++ src/components/Header/HeaderSec.jsx
@@ -3,66 +3,66 @@ import djLogo from "../../Logos/DJ.svg";
 const HeaderSec = () => {
   return (
     <header className=" w-full absolute top-0">
-    <nav className=" border-gray-200 px-4 lg:px-6 py-2.5 dark:bg-gray-800">
-      <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
-        <div className="flex flex-col">
-          <button>
-            <MenuIcon color="white" />
-          </button>
-          <span className="self-center text-xl font-semibold whitespace-nowrap text-white">
-            Menu
-          </span>
-        </div>
-        <div className="flex items-center lg:order-2">
-          <button className="text-white bg-custom-brown font-light px-4 lg:px-8 py-0.5 lg:py-1.5">
-            <div className="flex flex-col">
-              <span>RESERVER</span>
-              <span className="text-xs ">Au meilleur tarif</span>
-            </div>
-          </button>
-          <button
-            data-collapse-toggle="mobile-menu-2"
-            type="button"
-            className="inline-flex items-center p-2 ml-1 text-sm text-gray-500 rounded-lg lg:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
-            aria-controls="mobile-menu-2"
-            aria-expanded="false"
+      <nav className=" border-gray-200 px-4 lg:px-6 py-2.5 dark:bg-gray-800">
+        <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
+          <div className="flex flex-col">
+            <button>
+              <MenuIcon color="white" />
+            </button>
+            <span className="self-center text-xl font-semibold whitespace-nowrap text-white">
+              Menu
+            </span>
+          </div>
+          <div className="flex items-center lg:order-2">
+            <button className="text-white bg-custom-brown font-light px-4 lg:px-8 py-0.5 lg:py-1.5">
+              <div className="flex flex-col">
+                <span>RESERVER</span>
+                <span className="text-xs ">Au meilleur tarif</span>
+              </div>
+            </button>
+            <button
+              data-collapse-toggle="mobile-menu-2"
+              type="button"
+              className="inline-flex items-center p-2 ml-1 text-sm text-gray-500 rounded-lg lg:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+              aria-controls="mobile-menu-2"
+              aria-expanded="false"
+            >
+              <span className="sr-only">Open main menu</span>
+              <svg
+                className="w-6 h-6"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
+                  clipRule="evenodd"
+                ></path>
+              </svg>
+              <svg
+                className="hidden w-6 h-6"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                  clipRule="evenodd"
+                ></path>
+              </svg>
+            </button>
+          </div>
+          <div
+            className="hidden justify-between items-center w-full lg:flex lg:w-auto lg:order-1"
+            id="mobile-menu-2"
           >
-            <span className="sr-only">Open main menu</span>
-            <svg
-              className="w-6 h-6"
-              fill="currentColor"
-              viewBox="0 0 20 20"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                fillRule="evenodd"
-                d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
-                clipRule="evenodd"
-              ></path>
-            </svg>
-            <svg
-              className="hidden w-6 h-6"
-              fill="currentColor"
-              viewBox="0 0 20 20"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                fillRule="evenodd"
-                d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-                clipRule="evenodd"
-              ></path>
-            </svg>
-          </button>
+            <img src={djLogo} height={100} width={100} alt="DJ-Logo" />
+          </div>
         </div>
-        <div
-          className="hidden justify-between items-center w-full lg:flex lg:w-auto lg:order-1"
-          id="mobile-menu-2"
-        >
-          <img src={djLogo} height={100} width={100} alt="DJ-Logo" />
-        </div>
-      </div>
-    </nav>
-  </header>
+      </nav>
+    </header>
   );
 };
 
