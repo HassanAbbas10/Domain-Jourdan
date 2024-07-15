8f97c7cc3f641ff483080b523c867fa610478441
Hassan Abbas <95937687+HassanAbbas10@users.noreply.github.com>
2024-07-15 15:01:26 +0500

    modified:   src/utils/Events.js 	new file:   src/utils/Pictures/first.jpg 	new file:   src/utils/Pictures/second.jpg 	new file:   src/utils/Pictures/third.jpg

--- src/utils/Events.js
+++ src/utils/Events.js
@@ -1,14 +1,19 @@
+import firstPic from '../utils/Pictures/first.jpg'
+import secondPic from '../utils/Pictures/second.jpg'
+import thirdPic from '../utils/Pictures/third.jpg'
 const eventData = [
     {id:1,
-    picture:,
+    picture:firstPic,
     desc:'Pour des séminaires journée ou demi-journée, Le Domaine dispose d’une salle de 30pers maximum en style école/ U',
     },
     {id:2,
-    picture:,
+    picture:secondPic,
     desc:'Le Domaine peut se privatiser du vendredi 18h au dimanche 16h, petit-déjeuner inclus...',
     },
     {id:3,
-    picture:,
+    picture:thirdPic,
     desc:'Déjeuner ou diner en extérieur, si beau temps jusqua  150pers Déjeuner ou dîner autour de la piscine.',
     },
 ]
+
+export default eventData;
