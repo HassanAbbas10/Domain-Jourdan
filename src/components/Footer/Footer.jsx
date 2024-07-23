65e3316ae36a0be997d5b604c83a29afcb3754a2
Hassan Abbas <95937687+HassanAbbas10@users.noreply.github.com>
2024-07-23 12:29:44 +0500

    modified:   src/components/Footer/Footer.jsx

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,8 +1,8 @@
 import Dj from "../../Logos/DJ.svg";
 import { Link } from "react-router-dom";
-import footer  from '../../Logos/footer.svg'
-import LN from '../../Logos/footerLn.svg'
-import instagram from '../../Logos/footerINS.svg'
+import footer from "../../Logos/footer.svg";
+import { Instagram } from "@mui/icons-material";
+import { LinkedIn } from "@mui/icons-material";
 const Footer = () => {
   return (
     <footer className="w-full bg-gray-900">
@@ -11,193 +11,64 @@ const Footer = () => {
           <img src={Dj} />
         </div>
 
-        <div className="grid grid-cols-2 lg:grid-cols-4 lg:gap-8 gap-12 pt-14 pb-20 max-w-md mx-auto md:max-w-xl lg:max-w-full">
-          <div className="block">
-            <h4 className="text-xl text-white font-bold mb-7">
-              Domain Jourdan
-            </h4>
-            <ul className="text-lg  transition-all duration-500">
-              <li className="mb-6">
-                <a
-                  href="javascript:;"
-                  className="text-gray-400 hover:text-white"
-                >
-                  Home
-                </a>
-              </li>
-              <li className="mb-6">
-                <a
-                  href="javascript:;"
-                  className=" text-gray-400 hover:text-white"
-                >
-                  About
-                </a>
-              </li>
-              <li className="mb-6">
-                <a
-                  href="javascript:;"
-                  className=" text-gray-400 hover:text-white"
-                >
-                  Pricing
-                </a>
-              </li>
-              <li className="mb-6">
-                <a
-                  href="javascript:;"
-                  className=" text-gray-400 hover:text-white"
-                >
-                  Features
-                </a>
-              </li>
-              <li>
-                <a
-                  href="javascript:;"
-                  className=" text-gray-400 hover:text-white"
-                >
-                  Pro Version
-                </a>
-              </li>
-            </ul>
-          </div>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 pt-14 pb-20 max-w-md mx-auto md:max-w-2xl lg:max-w-full text-center">
+      <div className="border-b-2 md:border-r-2 md:border-b-0 border-gray-600 pb-6 md:pb-0">
+        <section className="text-lg mx-auto transition-all duration-500 space-y-5">
+          <p className="font-quicksand">
+            <a href="#" className="text-gray-400 hover:text-white duration-500">
+              610 chemin de la chapelle 13610 Le Puy-Sainte-Réparade
+            </a>
+          </p>
+          <p className="text-gray-400 hover:text-white duration-500">
+            07 82 00 23 24
+          </p>
+          <p className="text-gray-400 hover:text-white duration-500">
+            contact@domainejourdan.com
+          </p>
+        </section>
+      </div>
 
-          <div className="block">
-            <h4 className="text-xl text-white font-bold mb-7">Products</h4>
-            <ul className="text-lg  transition-all duration-500">
-              <li className="mb-6">
-                <a
-                  href="javascript:;"
-                  className="text-gray-400 hover:text-white"
-                >
-                  Figma UI System
-                </a>
-              </li>
-              <li className="mb-6">
-                <a
-                  href="javascript:;"
-                  className=" text-gray-400 hover:text-white"
-                >
-                  Icons Assets
-                </a>
-              </li>
-              <li className="mb-6">
-                <a
-                  href="javascript:;"
-                  className=" text-gray-400 hover:text-white"
-                >
-                  Responsive Blocks
-                </a>
-              </li>
-              <li className="mb-6">
-                <a
-                  href="javascript:;"
-                  className=" text-gray-400 hover:text-white"
-                >
-                  Components Library
-                </a>
-              </li>
-              <li>
-                <a
-                  href="javascript:;"
-                  className=" text-gray-400 hover:text-white"
-                >
-                  Plugin Guide
-                </a>
-              </li>
-            </ul>
-          </div>
+      <div className="border-b-2 md:border-r-2 md:border-b-0 border-gray-600 pb-6 md:pb-0">
+        <section className="text-lg mx-auto transition-all duration-500 space-y-2">
+          <p className="font-quicksand">
+            <Link
+              to="/contact"
+              className="text-gray-400 hover:text-white duration-500"
+            >
+              Contact & Accès
+            </Link>
+          </p>
+          <p className="text-gray-400 hover:text-white duration-500">
+            Ouvert tous les jours 24/24
+          </p>
+        </section>
+      </div>
 
-          <div className="block">
-            <h4 className="text-xl text-white font-bold mb-7">Resources</h4>
-            <ul className="text-lg  transition-all duration-500">
-              <li className="mb-6">
-                <a
-                  href="javascript:;"
-                  className="text-gray-400 hover:text-white"
-                >
-                  FAQs
-                </a>
-              </li>
-              <li className="mb-6">
-                <a
-                  href="javascript:;"
-                  className=" text-gray-400 hover:text-white"
-                >
-                  Quick Start
-                </a>
-              </li>
-              <li className="mb-6">
-                <a
-                  href="javascript:;"
-                  className=" text-gray-400 hover:text-white"
-                >
-                  Documentation
-                </a>
-              </li>
-              <li className="mb-6">
-                <a
-                  href="javascript:;"
-                  className=" text-gray-400 hover:text-white"
-                >
-                  User Guide
-                </a>
-              </li>
-              <li>
-                <a
-                  href="javascript:;"
-                  className=" text-gray-400 hover:text-white"
-                >
-                  Plugin Guide
-                </a>
-              </li>
-            </ul>
-          </div>
-
-          <div className="block">
-            <h4 className="text-xl text-white font-bold mb-7">Support</h4>
-            <ul className="text-lg  transition-all duration-500">
-              <li className="mb-6">
-                <a
-                  href="javascript:;"
-                  className="text-gray-400 hover:text-white"
-                >
-                  Customer Support
-                </a>
-              </li>
-              <li className="mb-6">
-                <a
-                  href="javascript:;"
-                  className=" text-gray-400 hover:text-white"
-                >
-                  Cookies
-                </a>
-              </li>
-              <li className="mb-6">
-                <a
-                  href="javascript:;"
-                  className=" text-gray-400 hover:text-white"
-                >
-                  License
-                </a>
-              </li>
-              <li className="mb-6">
-                <a
-                  href="javascript:;"
-                  className=" text-gray-400 hover:text-white"
-                >
-                  Terms & Conditions
-                </a>
-              </li>
-              <li>
-                <a
-                  href="javascript:;"
-                  className=" text-gray-400 hover:text-white"
-                >
-                  Privacy Policy
-                </a>
-              </li>
-            </ul>
-          </div>
-        </div>
+      <div>
+        <section className="text-lg mx-auto transition-all duration-500 space-y-3">
+          <p className="font-quicksand">
+            <Link
+              to="/chambres"
+              className="text-gray-400 hover:text-white duration-500"
+            >
+              Hotel & chambres
+            </Link>
+          </p>
+          <p className="text-gray-400 hover:text-white duration-500">
+            Restaurant & bar
+          </p>
+          <p className="text-gray-400 hover:text-white duration-500">
+            Séminaire
+          </p>
+          <p className="text-gray-400 hover:text-white duration-500">
+            Evènement privé
+          </p>
+          <p className="text-gray-400 hover:text-white duration-500">
+            Galerie
+          </p>
+        </section>
+      </div>
+    </div>
 
         <div className="py-7 border-t border-gray-700 ">
           <div className="flex items-center justify-center flex-col lg:space-y-0 space-y-8 lg:justify-between lg:flex-row">
@@ -209,26 +80,24 @@ const Footer = () => {
               Politique de confidentialité & Mentions Légales
             </span>
 
-            <img src={footer}/>
+            <img src={footer} />
 
             <span className="text-gray-400 font-quicksand ">
-            Conditions générales de vente
+              Conditions générales de vente
             </span>
             <div className="flex mt-4 space-x-4 sm:justify-center sm:mt-0 ">
               <a
                 href="javascript:;"
-                className="w-14 h-14 border flex justify-center items-center hover:"
+                className="w-14 h-14 border rounded-full border-gray-400 flex justify-center items-center hover:shadow-lg hover:shadow-custom-brown duration-500"
               >
-                <img src={LN} width={50}/>
+                <LinkedIn sx={{ color: "gray" }} />
               </a>
               <a
                 href="javascript:;"
-                className="w-12 h-14 flex justify-center items-center hover:"
+                className="w-14 h-14 border rounded-full border-gray-400 flex justify-center items-center hover:shadow-lg hover:shadow-custom-brown duration-500"
               >
-               <img src={instagram} width={50}/>
+                <Instagram sx={{ color: "gray" }} />
               </a>
-              
-               
             </div>
           </div>
         </div>
